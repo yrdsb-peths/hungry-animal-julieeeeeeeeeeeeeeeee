86bf1b7d865bdf24cb5cef35ae7f04cf29cd1a86
julieeeeeeeeeeeeeeeee <341081727@gapps.yrdsb.ca>
2024-09-19 12:58:01 -0400

    Add seal

---
+++ Seal.java
@@ -0,0 +1,9 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+public class Seal extends Actor
+{
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
