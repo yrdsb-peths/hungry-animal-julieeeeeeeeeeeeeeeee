49714b80f45f6d936163bd6da6f746d4b1127558
julieeeeeeeeeeeeeeeee <341081727@gapps.yrdsb.ca>
2024-09-21 11:35:53 -0400

    Add sprite animations for left and right

--- Elephant.java
+++ Elephant.java
@@ -3,11 +3,42 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Elephant extends Actor
 {
     GreenfootSound elephantSound = new GreenfootSound("elephantcub.mp3");
-    GreenfootImage idle = new GreenfootImage("images/elephant_idle/idle0.png");
+    GreenfootImage[] idleRight = new GreenfootImage[8];
+    GreenfootImage[] idleLeft = new GreenfootImage[8];
+    
+    String facing = "right";
     
     public Elephant()
     {
-        setImage(idle);
+        for(int i = 0; i < idleRight.length; i++)
+        {
+            idleRight[i] = new GreenfootImage("images/elephant_idle/idle" + i + ".png");
+            idleRight[i].scale(100, 100);
+        }
+        
+        for(int i = 0; i <idleLeft.length; i++)
+        {
+           idleLeft[i] = new GreenfootImage("images/elephant_idle/idle" + i + ".png");
+           idleLeft[i].mirrorHorizontally();
+           idleLeft[i].scale(100, 100);
+        }
+        
+        setImage(idleRight[0]);
+    }
+    
+    int imageIndex = 0;
+    public void animateElephant()
+    {
+        if(facing.equals("right"))
+        {
+            setImage(idleRight[imageIndex]);
+            imageIndex = (imageIndex + 1) % idleRight.length;
+        }
+        else
+        {
+            setImage(idleLeft[imageIndex]);
+            imageIndex = (imageIndex + 1) % idleLeft.length;
+        }
     }
     
     public void act()
@@ -15,13 +46,17 @@ public class Elephant extends Actor
         if(Greenfoot.isKeyDown("left"))
         {
             move(-3);
+            facing = "left";
         }
         if(Greenfoot.isKeyDown("right"))
         {
             move(3);
+            facing = "right";
         }
         
         eat();
+        
+        animateElephant();
     }
     
     public void eat()
