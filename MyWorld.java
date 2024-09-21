240915fa63afcf159174213a14d35637c2c62452
julieeeeeeeeeeeeeeeee <341081727@gapps.yrdsb.ca>
2024-09-21 10:54:47 -0400

    Add game over method

--- MyWorld.java
+++ MyWorld.java
@@ -18,6 +18,12 @@ public class MyWorld extends World
         createCupcake();
     }
     
+    public void gameOver()
+    {
+        Label gameOverLabel = new Label("Game Over", 100);
+        addObject(gameOverLabel, 300, 200);
+    }
+    
     public void increaseScore()
     {
         score++;
