f3db8ce0f10786cfef16198773bab8efac47e25e
julieeeeeeeeeeeeeeeee <341081727@gapps.yrdsb.ca>
2024-09-21 12:02:33 -0400

    Add levels and speed

--- Apple.java
+++ Apple.java
@@ -2,10 +2,12 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class Apple extends Actor
 {
+    private int speed = 1;
+    
     public void act()
     {
         int x = getX();
-        int y = getY() + 2;
+        int y = getY() + speed;
         setLocation(x, y);
         
         MyWorld world = (MyWorld) getWorld();
@@ -14,5 +16,10 @@ public class Apple extends Actor
             world.gameOver();
             world.removeObject(this);
         }
+    }
+    
+    public void setSpeed(int spd)
+    {
+        speed = spd;
     }
 }
